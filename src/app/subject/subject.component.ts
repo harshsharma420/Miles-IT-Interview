f24812d79701b6b6bdc4cde351bd7490aab9b219
Harsh Sharma <sharmaharsh555@gmail.com>
2024-04-09 23:23:29 +0530

    Implemented subject

--- src/app/subject/subject.component.ts
+++ src/app/subject/subject.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-subject',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./subject.component.scss']
 })
 export class SubjectComponent {
+  first_subscriber_subject?: number;
+  second_subscriber_subject?: number;
+  third_subscriber_subject?: number;
+  subject = new Subject<number>();
 
+  firstSubscribe = this.subject.subscribe({
+    next:
+      result => {
+        this.first_subscriber_subject = result;
+        console.log(result);
+      }
+  }
+  );
+  secondSubscribe = this.subject.subscribe({
+    next: result => {
+      this.second_subscriber_subject = result;
+      console.log(result);
+    }
+  })
+
+  thirdSubscribe = this.subject.subscribe({
+    next: result => {
+      this.third_subscriber_subject = result;
+      console.log(result);
+    }
+  })
+
+  firstNext = this.subject.next(Math.random() * 1000);
+  secondNext = this.subject.next(Math.random() * 10);
+  thirdNext = this.subject.next(Math.random() * 10000000);
+  fourthNext?: any;
+
+  nextSubject() {
+    this.fourthNext = this.subject.next(Math.random());
+  }
 }
