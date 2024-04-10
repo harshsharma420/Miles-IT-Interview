12adb78f84a1d82034a69fc5a0f428681389861f
Harsh Sharma <sharmaharsh555@gmail.com>
2024-04-10 23:24:06 +0530

    Added some custom functions to the behaviorSubject Component

--- src/app/behavior-subject/behavior-subject.component.ts
+++ src/app/behavior-subject/behavior-subject.component.ts
@@ -10,24 +10,38 @@ export class BehaviorSubjectComponent {
   behaviorSubject = new BehaviorSubject<string>('hello');
 
   nextValue = this.behaviorSubject.next('asd');
-  subscription:any;
+  subscription: any;
   subscribe?: string;
-  ngOnInit(){
+  ngOnInit() {
     this.subscription = this.behaviorSubject.subscribe({
-      next : res => {
+      next: res => {
         console.log(res);
         this.subscribe = res;
       }, error: err => {
         console.error(err);
-      },complete: () => {
+      }, complete: () => {
         console.log("behaviorComplete")
       }
     });
 
   }
 
-  ngOnDestroy(){
+  
+  ngOnDestroy() {
     this.subscription.unsubscribe();
   }
+  nextValueButton(){
+    this.nextValue = this.behaviorSubject.next(this.generateString(5))
+  }
+  characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
+  generateString(length:any) {
+      let result = ' ';
+      const charactersLength = this.characters.length;
+      for ( let i = 0; i < length; i++ ) {
+          result += this.characters.charAt(Math.floor(Math.random() * charactersLength));
+      }
+  
+      return result;
+  }
 }
