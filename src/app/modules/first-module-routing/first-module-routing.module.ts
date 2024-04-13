3223fecdcf8fa6f2992371a6d8ff8a1e7d9df175
Harsh Sharma <sharmaharsh555@gmail.com>
2024-04-13 23:36:13 +0530

    Configured the route module

--- src/app/modules/first-module-routing/first-module-routing.module.ts
+++ src/app/modules/first-module-routing/first-module-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
+import { RouterModule , Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FirstModuleComponentComponent } from '../first-module/first-module-component/first-module-component.component';
 
-
+const routes: Routes = [{ path: '', component: FirstModuleComponentComponent }];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class FirstModuleRoutingModule { }
