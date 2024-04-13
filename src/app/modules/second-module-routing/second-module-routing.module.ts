4c494af12ffc07c6988159135dd476d34ee9a266
Harsh Sharma <sharmaharsh555@gmail.com>
2024-04-13 23:49:43 +0530

    Configured the second routing module

--- src/app/modules/second-module-routing/second-module-routing.module.ts
+++ src/app/modules/second-module-routing/second-module-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { SecondModuleComponentComponent } from '../second-module/second-module-component/second-module-component.component';
 
+const routes: Routes = [{ path: '', component: SecondModuleComponentComponent }];
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class SecondModuleRoutingModule { }
