88de2ee65587bdd76cb79dbbd74f052aad93eb7b
Harsh Sharma <sharmaharsh555@gmail.com>
2024-04-17 23:58:23 +0530

    Wrote business logic for filter operator

--- src/app/filter-operator/filter-operator.component.ts
+++ src/app/filter-operator/filter-operator.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
-
+import { filter, of } from 'rxjs';
 @Component({
   selector: 'app-filter-operator',
   templateUrl: './filter-operator.component.html',
   styleUrls: ['./filter-operator.component.scss']
 })
 export class FilterOperatorComponent {
+  users = [
+    {
+      id: '1',
+      name: 'John',
+      isActive: true
+    },
+    {
+      id: '2',
+      name: 'Markesh',
+      isActive: true
+    },
+    {
+      id: '3',
+      name: 'Messiah',
+      isActive: true
+    }
+  ];
 
+  users$ = of(this.users);
+  filteredUsers$ = this.users$.pipe(filter(value => value.every(user => user.isActive)))
+
+  ngOnInit(){
+    this.filteredUsers$.subscribe(console.log);
+  }
 }
